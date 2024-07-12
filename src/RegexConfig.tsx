074ea2b432220e8aa28500fb0af8bdb8bc4fb11c
Saloni Oswal <45456510+sallyoswal@users.noreply.github.com>
2024-07-11 22:57:21 -0700

    Update RegexConfig.tsx

--- src/RegexConfig.tsx
+++ src/RegexConfig.tsx
@@ -2,5 +2,3 @@
 export const regexConfig = {
   namePattern: new RegExp(process.env.REACT_APP_OKTA_APP_NAME_PATTERN || '^[A-Z][A-Za-z0-9-]*$'),
 };
-
-console.log(`Loaded REACT_APP_OKTA_APP_NAME_PATTERN: ${process.env.REACT_APP_OKTA_APP_NAME_PATTERN}`);
